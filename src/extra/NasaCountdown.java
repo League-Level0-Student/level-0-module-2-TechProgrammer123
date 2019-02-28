000ae3b7a3f201b84333635ccf21478fc3f47ce2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-27 18:26:53 -0800

    Coding Random

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -15,13 +17,16 @@ package extra;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+String CountDown = JOptionPane.showInputDialog("Where do you want to start counting down from?(1,2,3,4, etc)");
 		// 3. Change the countdown to use the new starting point
-		
+		int Count = Integer.parseInt(CountDown);
 		// 1. Print a countdown from 10 to 0 on the console
-	
+	for (int i = Count; i >= 0; i--) {
+		System.out.println(i);
+		speak(i+"");
+	}
 			// 4. Use the speak method to hear the countdown.
-		
+		speak("Blast off!");
 			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
 		
 		// 5. when the counting is done, speak "blastoff!"
